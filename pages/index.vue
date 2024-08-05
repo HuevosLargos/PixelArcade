6b0d95170936d3f1d48cc65f15867a663178ee62
HuevosLargos <fornite366swich@gmail.com>
2024-08-05 04:24:46 +0200

    Segundo commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,22 @@
 <template>
-  <p>
-    Pagina Principal
-  </p>
+  <div id="fondo">
+    <p id="texto">
+      Pagina Principal
+    </p>
+  </div>
 </template>
 <script>
 
 </script>
+<style>
+#texto {
+  color: aquamarine;
+}
+
+#fondo {
+  background-color: grey;
+  padding: 21.5%;
+  text-align: center;
+  font-size: 32px;
+}
+</style>
